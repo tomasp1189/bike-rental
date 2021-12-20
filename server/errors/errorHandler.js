58aef6d8f4b9c063455917beb8de9b45a13e3b62
Tomas Puricelli <tomasp1189@gmail.com>
2021-12-20 10:45:56 +0100

    Add custom error and update errorHandler

--- server/errors/errorHandler.js
+++ server/errors/errorHandler.js
@@ -5,26 +5,38 @@ export default function errorHandler(handler) {
     try {
       return await handler(req, res);
     } catch (err) {
-      console.error(err.name, err);
+      console.error(err.stack);
       switch (err.name) {
         case errorNames.VALIDATION_ERROR:
         case errorNames.BAD_REQUEST:
-          return res.status(httpStatusCodes.BAD_REQUEST).json({ message: err });
+          return res
+            .status(httpStatusCodes.BAD_REQUEST)
+            .json({ success: false, message: err.message });
         case errorNames.UNAUTHORIZED_ERROR:
           return res
             .status(httpStatusCodes.UNAUTHORIZED)
-            .json({ message: err.message || 'Unauthorized request' });
+            .json({
+              success: false,
+              message: err.message || 'Unauthorized request',
+            });
         case errorNames.FORBIDDEN:
           return res
             .status(httpStatusCodes.FORBIDDEN)
-            .json({ message: err.message || 'Forbidden request' });
+            .json({
+              success: false,
+              message: err.message || 'Forbidden request',
+            });
         case errorNames.NOT_FOUND:
           return res.status(httpStatusCodes.NOT_FOUND).json({ message: err });
+        case errorNames.CONFLICT:
+          return res
+            .status(httpStatusCodes.CONFLICT)
+            .json({ success: false, message: err.message });
         case errorNames.INTERNAL_SERVER:
         default:
           return res
             .status(httpStatusCodes.INTERNAL_SERVER)
-            .json({ message: err.message });
+            .json({ success: false, message: 'Internal Server Error' });
       }
     }
   };
